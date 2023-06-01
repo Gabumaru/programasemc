7aba6c86f85abaad42814e9fc3d95509b6499ffd
Gabriel Jefferson <135045027+Gabumaru@users.noreply.github.com>
2023-06-01 10:42:54 -0300

    renomeando programas

--- exercicio2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main (void){
-int fim=1, b=2, c=3;
-printf("Os numeros sao: %d, %d, e %d\n",fim,b,c);
-	
-	
-}
--- exercicio3.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-int main(void){
-printf("%s\n%s\n%s","um","dois","tres");
-}
